598f917b1ad335b0b248e79f9c40ba6a09cd4973
CaptainSqrBeard <iliya.11.levchenko@gmail.com>
2022-04-27 16:42:49 +0600

    Add: имуннитет

--- Lua/Autorun/main.lua
+++ Lua/Autorun/main.lua
@@ -7,6 +7,7 @@ if Game.IsMultiplayer and SERVER or not Game.IsMultiplayer then
     for _, value in pairs(IS_ENABLED) do
         if (value.Name == "AMlua") then
             require("Scripts.blood")
+            require("Scripts.immunity")
             require("Scripts.hypothermia")
             require("Scripts.debug")
             require("Scripts.update")
