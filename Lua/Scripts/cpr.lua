a47de7888e309e88e51e028a035f862551f1810e
hnappinn <samnep1@mail.ru>
2022-04-27 21:36:08 +0300

    Revert "Update cpr.lua"
    This reverts commit 139dc53e906479303c762c8a68a10fd201ad53ec.

--- Lua/Scripts/cpr.lua
+++ Lua/Scripts/cpr.lua
@@ -8,8 +8,7 @@ end)
 Hook.Add("human.CPRFailed", "AM.CPRFailed", function(animcontroller)
     if animcontroller==nil or animcontroller.Character==nil or animcontroller.Character.SelectedCharacter==nil then return end
     local character = animcontroller.Character.SelectedCharacter
-    
-    HF.AddAfflictionLimb(character,LimbType.Torso)
+
     if(HF.Chance(0.01*)) then
         HF.AddAffliction(character,"t_fracture",1)
     if(HF.Chance(0.001)) then
