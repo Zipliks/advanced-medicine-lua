c04a355fca2653a669e3fdddfda189887b8e6fc6
CaptainSqrBeard <iliya.11.levchenko@gmail.com>
2022-05-02 21:13:25 +0600

    FIX: названия комманд

--- Lua/Scripts/testing.lua
+++ Lua/Scripts/testing.lua
@@ -4,7 +4,7 @@ Main.TestingEnabled = true
 
 Hook.Add('chatMessage', 'AM.testing', function(msg, client)
     
-    if (msg == 'Main test') then -- a glorified suicide button
+    if (msg == 'AM test') then -- a glorified suicide button
 
         if(client.Character == nil) then return true end
         Main.TraumamputateLimb(client.Character,LimbType.RightArm)
@@ -13,7 +13,7 @@ Hook.Add('chatMessage', 'AM.testing', function(msg, client)
         Main.TraumamputateLimb(client.Character,LimbType.LeftLeg)
 
         return true -- hide message
-    elseif (msg == 'Main unfuck') then -- a command to remove non-sensical extremity amputations on the head and torso
+    elseif (msg == 'AM unfuck') then -- a command to remove non-sensical extremity amputations on the head and torso
 
         if(client.Character == nil) then return true end
 
@@ -28,7 +28,7 @@ Hook.Add('chatMessage', 'AM.testing', function(msg, client)
         Utils.SetAfflictionLimb(client.Character,"tra_amputation",LimbType.Torso,0)
 
         return true -- hide message
-    elseif(msg=="nt1") then
+    elseif(msg=="am1") then
         if not Main.TestingEnabled then return end
         -- insert testing stuff here
         
@@ -39,7 +39,7 @@ Hook.Add('chatMessage', 'AM.testing', function(msg, client)
         end
 
         return true
-    elseif(msg=="nt2") then
+    elseif(msg=="am2") then
         if not Main.TestingEnabled then return end
         -- insert other testing stuff here
         local crewenum = Character.GetFriendlyCrew(client.Character)
