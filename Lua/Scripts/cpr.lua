e09211921778cc1f3e67da53969437d12b850b11
Zipliks <zipliks@gmail.com>
2022-11-12 20:24:08 +0300

    fuck `organdamage`, all my homies use `internaldamage`
    We are able to set afflictions directly, hence remove redundant `cpr_buff` affliction.

--- Lua/Scripts/cpr.lua
+++ Lua/Scripts/cpr.lua
@@ -1,13 +1,19 @@
 Hook.Add("human.CPRSuccess", "CPRSuccess", function(animcontroller)
-    if (animcontroller == nil) or (animcontroller.Character == nil) or (animcontroller.Character.SelectedCharacter == nil) then return end
-    local character = animcontroller.Character.SelectedCharacter
-    
-    Utils.SetAffliction(character, "cpr_buff", 2, nil, false)
+	if (animcontroller == nil) or (animcontroller.Character == nil) or (animcontroller.Character.SelectedCharacter == nil) then
+		return
+	end
+	local character = animcontroller.Character.SelectedCharacter
+
+	Utils.SetAffliction(character, "oxygenlow", -5, nil, true)
+	Utils.SetAffliction(character, "asystole", -3, nil, true)
 end)
 
 Hook.Add("human.CPRFailed", "CPRFailed", function(animcontroller)
-    if (animcontroller == nil) or (animcontroller.Character == nil) or (animcontroller.Character.SelectedCharacter == nil) then return end
-    local character = animcontroller.Character.SelectedCharacter
+	if (animcontroller == nil) or (animcontroller.Character == nil) or (animcontroller.Character.SelectedCharacter == nil) then
+		return
+	end
 
-    Utils.SetAffliction(character, "blunttrauma", 0.3, LimbType.Torso, true)
+	local character = animcontroller.Character.SelectedCharacter
+
+	Utils.SetAffliction(character, "internaldamage", 2, LimbType.Torso, true)
 end)
