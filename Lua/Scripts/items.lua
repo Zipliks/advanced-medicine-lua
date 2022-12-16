4c7a2bb775bc1aac3df3a366e07cd2d3dc66ca7c
Zipliks <zipliks@gmail.com>
2022-12-16 03:12:46 +0300

    format document

--- Lua/Scripts/items.lua
+++ Lua/Scripts/items.lua
@@ -86,119 +86,120 @@ end)
 -- TODO: Сделать чтобы они триггерились и вне меню (Или смирится с этим, лол)
 -- Zipliks: Пока забить и смириться
 Main.SetItemFunction("antibleeding1", function(item, usingCharacter, targetCharacter, limb)
-    print("User: " .. usingCharacter.Name .. " Target: " .. targetCharacter.Name)
+	print("User: " .. usingCharacter.Name .. " Target: " .. targetCharacter.Name)
 end)
 
 
-Main.SetItemFunction("defib", function(item, usingCharacter, targetCharacter, limb) 
-    print("User: " .. usingCharacter.Name .. " Target: " .. targetCharacter.Name)
+Main.SetItemFunction("defib", function(item, usingCharacter, targetCharacter, limb)
+	print("User: " .. usingCharacter.Name .. " Target: " .. targetCharacter.Name)
 
-    if item.Condition <= 0 then return end
+	if item.Condition <= 0 then return end
 
-    if limb ~= LimbType.Torso then return end
+	if limb ~= LimbType.Torso then return end
 
-    local containedItem = item.OwnInventory.GetItemAt(0)
-    if containedItem==nil then return end
-    local hasVoltage = containedItem.Condition > 0
+	local containedItem = item.OwnInventory.GetItemAt(0)
+	if containedItem == nil then return end
+	local hasVoltage = containedItem.Condition > 0
 
-    if hasVoltage then 
-        local actionRequired =
-        Utils.GetAffliction(targetCharacter,"ventricular_tachy") > 0
-            or Utils.GetAffliction(targetCharacter,"ventricular_fib") > 0
-            or Utils.GetAffliction(targetCharacter,"heartattack") > 0 
+	if hasVoltage then
+		local actionRequired =
+		Utils.GetAffliction(targetCharacter, "ventricular_tachy") > 0
+			or Utils.GetAffliction(targetCharacter, "ventricular_fib") > 0
+			or Utils.GetAffliction(targetCharacter, "heartattack") > 0
 
 
-        if not actionRequired then
-            -- HF.GiveItem(targetCharacter,"ntsfx_defib2")            -- TODO: Add SFX 
-        else
-            -- HF.GiveItem(targetCharacter,"ntsfx_defib1")            -- TODO: Add SFX 
+		if not actionRequired then
+			-- HF.GiveItem(targetCharacter,"ntsfx_defib2")            -- TODO: Add SFX
+		else
+			-- HF.GiveItem(targetCharacter,"ntsfx_defib1")            -- TODO: Add SFX
 
-            containedItem.Condition = containedItem.Condition-10
-            if containedItem.Prefab.Identifier.Value ~= "fulguriumbatterycell" then containedItem.Condition = containedItem.Condition-10 end
-    
-            local arrestSuccessChance = Utils.GetSkillLevel(usingCharacter,"medical")
+			containedItem.Condition = containedItem.Condition - 10
+			if containedItem.Prefab.Identifier.Value ~= "fulguriumbatterycell" then containedItem.Condition = containedItem.Condition
+				- 10 end
 
-            if Utils.Probabilty(arrestSuccessChance) then
-                Timer.Wait(function()
-                    Utils.SetAffliction(targetCharacter, "stun", 2, LimbType.Torso, true, usingCharacter)
-                    
-                    Utils.SetAffliction(targetCharacter, "ventricular_tachy",   0, LimbType.Torso, false, usingCharacter)
-                    Utils.SetAffliction(targetCharacter, "ventricular_fib",     0, LimbType.Torso, false, usingCharacter)
-                    Utils.SetAffliction(targetCharacter, "heartattack",         0, LimbType.Torso, false, usingCharacter)
-                end, 3200)
-            end
-        end
-    end
+			local arrestSuccessChance = Utils.GetSkillLevel(usingCharacter, "medical")
+
+			if Utils.Probabilty(arrestSuccessChance) then
+				Timer.Wait(function()
+					Utils.SetAffliction(targetCharacter, "stun", 2, LimbType.Torso, true, usingCharacter)
+
+					Utils.SetAffliction(targetCharacter, "ventricular_tachy", 0, LimbType.Torso, false, usingCharacter)
+					Utils.SetAffliction(targetCharacter, "ventricular_fib", 0, LimbType.Torso, false, usingCharacter)
+					Utils.SetAffliction(targetCharacter, "heartattack", 0, LimbType.Torso, false, usingCharacter)
+				end, 3200)
+			end
+		end
+	end
 end)
 
-Main.SetItemFunction("ambubag", function(item, usingCharacter, targetCharacter, limb) 
-    print("User: " .. usingCharacter.Name .. " Target: " .. targetCharacter.Name)
-    if item.Condition <= 0 then return end
+Main.SetItemFunction("ambubag", function(item, usingCharacter, targetCharacter, limb)
+	print("User: " .. usingCharacter.Name .. " Target: " .. targetCharacter.Name)
+	if item.Condition <= 0 then return end
 
-    if limb ~= LimbType.Head then return end
+	if limb ~= LimbType.Head then return end
 
-    local containedItem = item.OwnInventory.GetItemAt(0)
-    if containedItem==nil then return end
-    local hasOxygen = containedItem.Condition > 0
+	local containedItem = item.OwnInventory.GetItemAt(0)
+	if containedItem == nil then return end
+	local hasOxygen = containedItem.Condition > 0
 
-    if hasOxygen then 
-        containedItem.Condition = containedItem.Condition-5
-        Utils.SetAffliction(targetCharacter, "lifesupport",   50, LimbType.Torso, false, usingCharacter)
-    end
+	if hasOxygen then
+		containedItem.Condition = containedItem.Condition - 5
+		Utils.SetAffliction(targetCharacter, "lifesupport", 50, LimbType.Torso, false, usingCharacter)
+	end
 end)
 
 
 --- TODO: НЕ БУДЕТ РАБОТАТЬ НОРМАЛЬНО, ПОТОМУ ЧТО НЕ ПОДДЕРЖИВАЕТСЯ НОРМАЛЬНАЯ РАБОТА ЛЕКАРСТВ ПРИ ИХ НАНЕСЕНИИ НЕ ЧЕРЕЗ ХИЛ МЕНЮ
-Main.SetItemFunction("antibiotics", function(item, usingCharacter, targetCharacter, limb) 
-    print("User: " .. usingCharacter.Name .. " Target: " .. targetCharacter.Name)
+Main.SetItemFunction("antibiotics", function(item, usingCharacter, targetCharacter, limb)
+	print("User: " .. usingCharacter.Name .. " Target: " .. targetCharacter.Name)
 
-    local SuccessChance = Utils.GetSkillLevel(usingCharacter,"medical")
+	local SuccessChance = Utils.GetSkillLevel(usingCharacter, "medical")
 
-    if Utils.Probabilty(SuccessChance) then
-        if Utils.GetAffliction(targetCharacter, "huskinfection") < 100 then
-            Utils.SetAfflictionTime(targetCharacter, "huskinfection", -0.5, LimbType.Torso, true, 60, usingCharacter)
-            Utils.SetAfflictionTime(targetCharacter, "sepsis", -4, LimbType.Torso, true, 60, usingCharacter)
-            Utils.SetAfflictionTime(targetCharacter, "infectedwound", -20, LimbType.Torso, true, 60, usingCharacter)
-            Utils.SetAffliction(targetCharacter, "huskinfectionresistance", 600, LimbType.Torso, false, usingCharacter)
-            
-        end
-        Utils.SetAfflictionTime(targetCharacter, "organdamage", 0.55, LimbType.Torso, true, 60, usingCharacter)
-    else
-        if Utils.GetAffliction(targetCharacter, "huskinfection") < 100 then
-            Utils.SetAfflictionTime(targetCharacter, "huskinfection", -0.25, LimbType.Torso, true, 60, usingCharacter)
-            Utils.SetAfflictionTime(targetCharacter, "sepsis", -2, LimbType.Torso, true, 60, usingCharacter)
-            Utils.SetAfflictionTime(targetCharacter, "infectedwound", -10, LimbType.Torso, true, 60, usingCharacter)
-            Utils.SetAffliction(targetCharacter, "huskinfectionresistance", 300, LimbType.Torso, false, usingCharacter)
-            
-        end
-        Utils.SetAfflictionTime(targetCharacter, "organdamage", 1, LimbType.Torso, true, 60, usingCharacter)
-    end
+	if Utils.Probabilty(SuccessChance) then
+		if Utils.GetAffliction(targetCharacter, "huskinfection") < 100 then
+			Utils.SetAfflictionTime(targetCharacter, "huskinfection", -0.5, LimbType.Torso, true, 60, usingCharacter)
+			Utils.SetAfflictionTime(targetCharacter, "sepsis", -4, LimbType.Torso, true, 60, usingCharacter)
+			Utils.SetAfflictionTime(targetCharacter, "infectedwound", -20, LimbType.Torso, true, 60, usingCharacter)
+			Utils.SetAffliction(targetCharacter, "huskinfectionresistance", 600, LimbType.Torso, false, usingCharacter)
+
+		end
+		Utils.SetAfflictionTime(targetCharacter, "organdamage", 0.55, LimbType.Torso, true, 60, usingCharacter)
+	else
+		if Utils.GetAffliction(targetCharacter, "huskinfection") < 100 then
+			Utils.SetAfflictionTime(targetCharacter, "huskinfection", -0.25, LimbType.Torso, true, 60, usingCharacter)
+			Utils.SetAfflictionTime(targetCharacter, "sepsis", -2, LimbType.Torso, true, 60, usingCharacter)
+			Utils.SetAfflictionTime(targetCharacter, "infectedwound", -10, LimbType.Torso, true, 60, usingCharacter)
+			Utils.SetAffliction(targetCharacter, "huskinfectionresistance", 300, LimbType.Torso, false, usingCharacter)
+
+		end
+		Utils.SetAfflictionTime(targetCharacter, "organdamage", 1, LimbType.Torso, true, 60, usingCharacter)
+	end
 end)
 
-Main.SetItemFunction("adrenaline", function(item, usingCharacter, targetCharacter, limb) 
-    print("User: " .. usingCharacter.Name .. " Target: " .. targetCharacter.Name)
+Main.SetItemFunction("adrenaline", function(item, usingCharacter, targetCharacter, limb)
+	print("User: " .. usingCharacter.Name .. " Target: " .. targetCharacter.Name)
 
-    local SuccessChance = Utils.GetSkillLevel(usingCharacter,"medical")
+	local SuccessChance = Utils.GetSkillLevel(usingCharacter, "medical")
 
-    if Utils.Probabilty(SuccessChance) then
-        if Utils.GetAffliction(targetCharacter, "huskinfection") < 100 then
-            Utils.SetAfflictionTime(targetCharacter, "huskinfection", -0.5, LimbType.Torso, true, 60, usingCharacter)
-            Utils.SetAfflictionTime(targetCharacter, "sepsis", -4, LimbType.Torso, true, 60, usingCharacter)
-            Utils.SetAfflictionTime(targetCharacter, "infectedwound", -20, LimbType.Torso, true, 60, usingCharacter)
-            Utils.SetAffliction(targetCharacter, "huskinfectionresistance", 600, LimbType.Torso, false, usingCharacter)
-            
-        end
-        Utils.SetAfflictionTime(targetCharacter, "organdamage", 0.55, LimbType.Torso, true, 60, usingCharacter)
-    else
-        if Utils.GetAffliction(targetCharacter, "huskinfection") < 100 then
-            Utils.SetAfflictionTime(targetCharacter, "huskinfection", -0.25, LimbType.Torso, true, 60, usingCharacter)
-            Utils.SetAfflictionTime(targetCharacter, "sepsis", -2, LimbType.Torso, true, 60, usingCharacter)
-            Utils.SetAfflictionTime(targetCharacter, "infectedwound", -10, LimbType.Torso, true, 60, usingCharacter)
-            Utils.SetAffliction(targetCharacter, "huskinfectionresistance", 300, LimbType.Torso, false, usingCharacter)
-            
-        end
-        Utils.SetAfflictionTime(targetCharacter, "organdamage", 1, LimbType.Torso, true, 60, usingCharacter)
-    end
+	if Utils.Probabilty(SuccessChance) then
+		if Utils.GetAffliction(targetCharacter, "huskinfection") < 100 then
+			Utils.SetAfflictionTime(targetCharacter, "huskinfection", -0.5, LimbType.Torso, true, 60, usingCharacter)
+			Utils.SetAfflictionTime(targetCharacter, "sepsis", -4, LimbType.Torso, true, 60, usingCharacter)
+			Utils.SetAfflictionTime(targetCharacter, "infectedwound", -20, LimbType.Torso, true, 60, usingCharacter)
+			Utils.SetAffliction(targetCharacter, "huskinfectionresistance", 600, LimbType.Torso, false, usingCharacter)
+
+		end
+		Utils.SetAfflictionTime(targetCharacter, "organdamage", 0.55, LimbType.Torso, true, 60, usingCharacter)
+	else
+		if Utils.GetAffliction(targetCharacter, "huskinfection") < 100 then
+			Utils.SetAfflictionTime(targetCharacter, "huskinfection", -0.25, LimbType.Torso, true, 60, usingCharacter)
+			Utils.SetAfflictionTime(targetCharacter, "sepsis", -2, LimbType.Torso, true, 60, usingCharacter)
+			Utils.SetAfflictionTime(targetCharacter, "infectedwound", -10, LimbType.Torso, true, 60, usingCharacter)
+			Utils.SetAffliction(targetCharacter, "huskinfectionresistance", 300, LimbType.Torso, false, usingCharacter)
+
+		end
+		Utils.SetAfflictionTime(targetCharacter, "organdamage", 1, LimbType.Torso, true, 60, usingCharacter)
+	end
 end)
 
 Main.SetItemFunction("antibleeding1", function(item, source, target, limb)
@@ -247,8 +248,8 @@ end)
 
 Main.SetItemFunction("suture", function(item, source, target, limb)
 	local limbtype = Utils.NormalizeLimbType(limb.type)
-	local suturable = {"incision", "ligature", "retraction"}
-	if(Utils.SetAffCondition(target, "retraction", 1, limbtype, suturable, 1, true)) then
+	local suturable = { "incision", "ligature", "retraction" }
+	if (Utils.SetAffCondition(target, "retraction", 1, limbtype, suturable, 1, true)) then
 		Utils.RemoveItem(item)
 		Utils.SetAfflictionTime(target, "stitches", 20, limbtype, false, 30)
 	end
